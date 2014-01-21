d4466befdb5321492c27c560966c5afdbd226ebd
Abhinav Sarje <abhinav.sarje@gmail.com>
2014-01-21 08:41:57 -0800

    updating saxs2 build file

--- build/build-saxs2.sh
+++ build/build-saxs2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scons --with-mpi --with-cuda --extrapath=/usr/local/openmpi-1.6.1,/usr/local/boost_1_49_0,/usr/local/cuda,/usr/local/hdf5-1.8.9,/usr/local/szip-2.1 -j 12
+scons --with-mpi --with-cuda --extrapath=/usr/local/openmpi-1.6.1,/usr/local/boost,/usr/local/cuda,/usr/local/hdf5-1.8.9,/usr/local/szip-2.1 -j 12
