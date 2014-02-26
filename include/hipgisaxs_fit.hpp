8c3104b9913903231ad79a850fbcde3bd268b8bf
Abhinav Sarje <abhinav.sarje@gmail.com>
2014-02-26 13:44:54 -0800

    header file

---
+++ include/hipgisaxs_fit.hpp
@@ -0,0 +1,35 @@
+/**
+ *  Project: HipGISAXS (High-Performance GISAXS)
+ *
+ *  File: hipgisaxs.hpp
+ *  Created: Jan 26, 2014
+ *  Modified: Wed 26 Feb 2014 08:20:41 AM PST
+ *
+ *  Author: Abhinav Sarje <asarje@lbl.gov>
+ *  Developers: Slim Chourou <stchourou@lbl.gov>
+ *              Abhinav Sarje <asarje@lbl.gov>
+ *              Elaine Chan <erchan@lbl.gov>
+ *              Alexander Hexemer <ahexemer@lbl.gov>
+ *              Xiaoye Li <xsli@lbl.gov>
+ *
+ *  Licensing: The HipGISAXS software is only available to be downloaded and
+ *  used by employees of academic research institutions, not-for-profit
+ *  research laboratories, or governmental research facilities. Please read the
+ *  accompanying LICENSE file before downloading the software. By downloading
+ *  the software, you are agreeing to be bound by the terms of this
+ *  NON-COMMERCIAL END USER LICENSE AGREEMENT.
+ */
+
+#ifndef __HIPGISAXS_FIT_HPP__
+#define __HIPGISAXS_FIT_HPP__
+
+#include <analyzer/hipgisaxs_fit_pounders.hpp>
+#include <analyzer/hipgisaxs_fit_pso.hpp>
+#include <analyzer/hipgisaxs_fit_bruteforce.hpp>
+
+#include <analyzer/distance_functions.hpp>
+#include <analyzer/objective_func.hpp>
+#include <analyzer/hipgisaxs_ana.hpp>
+
+
+#endif /* __HIPGISAXS_FIT_HPP__ */
