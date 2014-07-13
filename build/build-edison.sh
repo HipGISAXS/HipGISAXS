7cbefd808d732154fe2313398ba68b8d02cde0b3
Abhinav Sarje <asarje@lbl.gov>
2014-07-13 08:52:36 -0700

    stuff for edison

--- build/build-edison.sh
+++ build/build-edison.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-module load boost szip cray-petsc
+#module load scons
+#module load gcc/4.8.2
+#module load boost szip cray-petsc
 
 scons --with-mpi --extrapath=/global/homes/a/asarje/local/hdf5-1.8.9-edison-intel,/global/homes/a/asarje/local/tiff-4.0.2-edison-intel,/global/homes/a/asarje/local/boost_1_49_0,/usr/common/usg/szip/2.1/intel -j 24
