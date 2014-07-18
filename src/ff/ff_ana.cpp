00fe644592f401ee19d2e9ecdad8568c85651325
Abhinav Sarje <abhinav.sarje@gmail.com>
2014-07-18 09:24:21 -0700

    rotation matrix xy swap thing

--- src/ff/ff_ana.cpp
+++ src/ff/ff_ana.cpp
@@ -3,7 +3,7 @@
  *
  *  File: ff_ana.cpp
  *  Created: Jul 12, 2012
- *  Modified: Sun 26 Jan 2014 10:21:36 AM PST
+ *  Modified: Fri 18 Jul 2014 08:53:08 AM PDT
  *
  *  Author: Abhinav Sarje <asarje@lbl.gov>
  *  Developers: Slim Chourou <stchourou@lbl.gov>
@@ -323,17 +323,17 @@ namespace hig {
 	    		                                complex_t& mqx, complex_t& mqy, complex_t& mqz) {
 		// FIXME: check which one is correct ...
 		// x and y swapped
-		/*mqx = qx * rot[0] + qy * rot[1] + qz * rot[2];
+		mqx = qx * rot[0] + qy * rot[1] + qz * rot[2];
 		mqy = qx * rot[3] + qy * rot[4] + qz * rot[5];
-		mqz = qx * rot[6] + qy * rot[7] + qz * rot[8];*/
+		mqz = qx * rot[6] + qy * rot[7] + qz * rot[8];
 		// original
-		mqx = qy * rot[0] + qx * rot[1] + qz * rot[2];
+		/*mqx = qy * rot[0] + qx * rot[1] + qz * rot[2];
 		mqy = qy * rot[3] + qx * rot[4] + qz * rot[5];
-		mqz = qy * rot[6] + qx * rot[7] + qz * rot[8];
+		mqz = qy * rot[6] + qx * rot[7] + qz * rot[8];*/
 		// rotation transposed
-		//mqx = qy * rot[0] + qx * rot[3] + qz * rot[6];
-		//mqy = qy * rot[1] + qx * rot[4] + qz * rot[7];
-		//mqz = qy * rot[2] + qx * rot[5] + qz * rot[8];
+		/*mqx = qx * rot[0] + qy * rot[3] + qz * rot[6];
+		mqy = qx * rot[1] + qy * rot[4] + qz * rot[7];
+		mqz = qx * rot[2] + qy * rot[5] + qz * rot[8];*/
 	} // AnalyticFormFactor::compute_meshpoints()
 
 } // namespace hig
