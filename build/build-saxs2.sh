e1c32ded6f834c0ee89cc88545d37530481f5829
Abhinav Sarje <asarje@lbl.gov>
2014-07-22 14:39:48 -0700

    build file for saxs2

--- build/build-saxs2.sh
+++ build/build-saxs2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scons --with-mpi --with-cuda --extrapath=/usr/local/openmpi-1.6.1,/usr/local/boost,/usr/local/cuda,/usr/local/hdf5-1.8.9,/usr/local/szip-2.1 -j 12
+scons --with-mpi --with-cuda --extrapath=/usr/local/openmpi-1.6.1,/usr/local/boost,/usr/local/cuda,/usr/local/hdf5-1.8.9,/usr/local/szip-2.1,/usr/local/tiff-4.0.3 -j 12
