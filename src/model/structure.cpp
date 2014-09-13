15e42ef02357d3f54462e9c299b2240002a8cab7
Abhinav Sarje <abhinav.sarje@gmail.com>
2014-09-13 07:06:52 -0700

    fixing iratio update

--- src/model/structure.cpp
+++ src/model/structure.cpp
@@ -190,10 +190,6 @@ namespace hig {
 		if(!extract_keyword_name_and_key(keyword, keyword_name, key)) return false;
 		std::string keyword2, rem_str2;
 		switch(TokenMapper::instance().get_keyword_token(keyword_name)) {
-			case struct_iratio_token:
-				iratio_ = new_val;
-				break;
-
 			case struct_ensemble_orient_rot1_token:
 				if(key.compare(std::string(1, rot1_.axis())) != 0) {
 					std::cerr << "error: invalid axis key given in param '" << str << "'" << std::endl;
@@ -404,6 +400,10 @@ namespace hig {
 		switch(TokenMapper::instance().get_keyword_token(keyword)) {
 			case key_token:
 				std::cerr << "warning: immutable param in '" << str << "'. ignoring." << std::endl;
+				break;
+
+			case struct_iratio_token:
+				iratio_ = new_val;
 				break;
 
 			case struct_grain_token:
