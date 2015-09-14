0f4adbe3def5a8fba6368af070352349d1574464
Abhinav Sarje <asarje@lbl.gov>
2015-09-14 10:45:41 -0700

    updating modules and build scripts for Edison.

--- build/build-edison.sh
+++ build/build-edison.sh
@@ -21,6 +21,6 @@ TIFFDIR=/global/homes/a/asarje/local/tiff-4.0.2-edison-intel
 
 #scons --with-mpi --extrapath=/global/homes/a/asarje/local/hdf5-1.8.9-edison-intel,/global/homes/a/asarje/local/tiff-4.0.2-edison-intel,/global/homes/a/asarje/local/boost_1_49_0,/usr/common/usg/szip/2.1/intel -j 24
 
-scons --with-mpi --extrapath=$BOOST_ROOT,$TIFFDIR -j12
-#scons --with-mpi --with-papi --extrapath=$BOOST_ROOT,$TIFFDIR -j12
+#scons --with-mpi --extrapath=$BOOST_ROOT,$TIFFDIR -j12
+scons --with-mpi --with-papi --extrapath=$BOOST_ROOT,$TIFFDIR -j12
 #scons --with-mpi --with-tau --extrapath=$BOOST_ROOT,$TIFFDIR -j12
