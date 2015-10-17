e81394c96c81194636ee709311f50018c5741cbd
Abhinav Sarje <asarje@lbl.gov>
2015-10-17 09:42:14 -0700

    edison build files

--- build/build-edison.sh
+++ build/build-edison.sh
@@ -9,6 +9,6 @@ TIFFDIR=/global/homes/a/asarje/local/tiff-4.0.2-edison-intel
 #export LD_LIBRARY_PATH=/global/homes/a/asarje/local/tau-2.24-edison/craycnl/lib:$LD_LIBRARY_PATH
 #export CRAY_LD_LIBRARY_PATH=/global/homes/a/asarje/local/tau-2.24-edison/craycnl/lib:$CRAY_LD_LIBRARY_PATH
 
-scons --with-mpi --extrapath=$BOOST_ROOT,$TIFFDIR -j12
+CC=cc CXX=CC scons --with-mpi --extrapath=$BOOST_ROOT,$TIFFDIR -j12
 #scons --with-mpi --with-papi --extrapath=$BOOST_ROOT,$TIFFDIR -j16
 #scons --with-mpi --with-tau --extrapath=$BOOST_ROOT,$TIFFDIR -j12
