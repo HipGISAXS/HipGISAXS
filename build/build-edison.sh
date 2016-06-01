8763298b597ef05197f99596ed989b2045e317b5
Abhinav Sarje <abhinav.sarje@gmail.com>
2016-06-01 14:09:16 -0700

    minor updates to edison build scripts

--- build/build-edison.sh
+++ build/build-edison.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-TIFFDIR=/global/homes/a/asarje/local/tiff-4.0.2-edison-intel
-
-## for TAU profiling
-#export TAU_MAKEFILE=${HOME}/local/tau-2.24-edison/craycnl/lib/Makefile.tau-intel-mpi
-#export TAU_OPTIONS="-optVerbose -optCompInst"
-#export PATH=/global/homes/a/asarje/local/tau-2.24-edison/craycnl/bin:$PATH
-#export LD_LIBRARY_PATH=/global/homes/a/asarje/local/tau-2.24-edison/craycnl/lib:$LD_LIBRARY_PATH
-#export CRAY_LD_LIBRARY_PATH=/global/homes/a/asarje/local/tau-2.24-edison/craycnl/lib:$CRAY_LD_LIBRARY_PATH
+#TIFFDIR=/global/homes/a/asarje/local/tiff-4.0.2-edison-intel
+TIFFDIR=/project/projectdirs/als/local/tiff-4.0.6
 
 CC=cc CXX=CC scons --with-mpi --extrapath=$BOOST_ROOT,$TIFFDIR -j12
-#scons --with-mpi --with-papi --extrapath=$BOOST_ROOT,$TIFFDIR -j16
-#scons --with-mpi --with-tau --extrapath=$BOOST_ROOT,$TIFFDIR -j12
