1abb2873e94ba7cf30d6021e6bf5db97c2299fc8
Abhinav Sarje <abhinav.sarje@gmail.com>
2016-07-05 12:35:50 -0700

    intel build script for andromeda

--- build/build-andromeda-intel.sh
+++ build/build-andromeda-intel.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-#scons --extrapath=/usr/local/openmpi,/usr/local/cuda,/usr/local/boost,/usr/local/hdf5-1.8.9-intel,/usr/local/tiff-4.0.2,/usr/local/szip-2.1 --with-mpi --with-cuda -j 32
-#CC=icc CXX=icpc scons --dbgo --extrapath=/usr/local/boost,/usr/local/tiff-4.0.2 -j 32
-CC=icc CXX=icpc scons --with-papi --extrapath=/usr/local/boost,/usr/local/tiff-4.0.2,/usr/local/papi-5.1.0 -j 32
+VTUNE_DIR=/opt/intel/vtune_amplifier_xe_2013
+BOOST_DIR=/usr/local/boost
+TIFF_DIR=/usr/local/tiff-4.0.2
+PAPI_DIR=/usr/local/papi-5.1.0
+
+EXTRA_DIRS=$VTUNE_DIR,$BOOST_DIR,$TIFF_DIR,$PAPI_DIR
+
+#CC=icc CXX=icpc scons --with-papi --extrapath=$EXTRA_DIRS -j 32
+CC=icc CXX=icpc scons --dbgo --with-papi --extrapath=$EXTRA_DIRS -j 32
